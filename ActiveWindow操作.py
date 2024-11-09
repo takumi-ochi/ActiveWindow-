4d150b227b20161ba0364a1ffde99e1230136df5
takumi-ochi <soryuusi01@gmail.com>
2024-11-10 06:22:32 +0900

    first commit

---
+++ ActiveWindow操作.py
@@ -0,0 +1,95 @@
+import wx
+import pygetwindow as gw
+import json
+import os
+
+CONFIG_FILE = 'config.json'
+
+class WindowSelector(wx.Frame):
+    def __init__(self, *args, **kw):
+        super(WindowSelector, self).__init__(*args, **kw)
+
+        self.panel = wx.Panel(self)
+        self.SetTitle('ウィンドウ選択')  # ウィンドウタイトルを設定
+
+        self.windows = gw.getAllTitles()
+        # 不要なウィンドウタイトルを除外
+        self.windows = [w for w in self.windows if w and w not in ["Windows 入力エクスペリエンス", "Program Manager", self.GetTitle()]]
+
+        if not self.windows:
+            wx.MessageBox('開いているウィンドウがありません。', '情報', wx.OK | wx.ICON_INFORMATION)
+            self.Close()
+            return
+
+        self.listbox = wx.ListBox(self.panel, choices=self.windows, style=wx.LB_SINGLE)
+        self.listbox.Bind(wx.EVT_LISTBOX_DCLICK, self.on_select)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.listbox, 1, wx.EXPAND | wx.ALL, 10)
+        self.panel.SetSizer(sizer)
+
+        self.load_config()
+
+        # フレーム全体でキーイベントをキャッチ
+        self.Bind(wx.EVT_CHAR_HOOK, self.on_key_down)
+
+        # ウィンドウが閉じられるときのイベントをバインド
+        self.Bind(wx.EVT_CLOSE, self.on_close)
+
+        # ウィンドウが非アクティブになったときのイベントをバインド
+        self.Bind(wx.EVT_ACTIVATE, self.on_activate)
+
+    def on_select(self, event):
+        self.activate_selected_window()
+
+    def on_key_down(self, event):
+        if event.GetKeyCode() == wx.WXK_RETURN:  # Enterキーが押された場合
+            self.activate_selected_window()
+        else:
+            event.Skip()  # 他のキーイベントを処理するためにスキップ
+
+    def activate_selected_window(self):
+        selected_title = self.listbox.GetStringSelection()
+        window = gw.getWindowsWithTitle(selected_title)
+        if window:
+            if window[0].isMinimized:
+                window[0].maximize()  # ウィンドウが最小化されている場合は最大化
+            window[0].activate()
+        self.Close()  # アプリケーションを終了
+
+    def load_config(self):
+        if os.path.exists(CONFIG_FILE):
+            with open(CONFIG_FILE, 'r') as f:
+                config = json.load(f)
+                size = tuple(config.get('size', (400, 300)))
+                self.SetSize(size)
+
+        # マウスカーソルの位置を取得してウィンドウの位置を設定
+        mouse_position = wx.GetMousePosition()
+        self.SetPosition(mouse_position)
+
+    def save_config(self):
+        config = {
+            'size': self.GetSize().Get(),  # wx.Sizeをタプルに変換
+        }
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(config, f)
+
+    def on_close(self, event):
+        self.save_config()
+        self.Destroy()  # ウィンドウを破棄してアプリケーションを終了
+
+    def on_activate(self, event):
+        if not event.GetActive():  # ウィンドウが非アクティブになった場合
+            self.Close()  # アプリケーションを終了
+        event.Skip()
+
+class WindowSwitcherApp(wx.App):
+    def OnInit(self):
+        self.frame = WindowSelector(None)
+        self.frame.Show()
+        return True
+
+if __name__ == '__main__':
+    app = WindowSwitcherApp()
+    app.MainLoop()
---
+++ exe作成.bat
@@ -0,0 +1,4 @@
+chcp 65001
+python -m PyInstaller ActiveWindow操作.py -F -w
+
+@echo off
